ff428d1e11e0a8f996af968ffe40e3fe83fdfc18
lxr <lxr@email.com>
2018-07-02 10:30:45 +0800

    refractor psh2.c

--- ch08/psh2.c
+++ ch08/psh2.c
@@ -41,7 +41,7 @@ void split2ArrByChar(const char *src, char splitchar, char ** arr, int *num)
 		if(*end == splitchar)
 		{
 			//printf("size %ld\n", end - start);
-			if(end - start > 0) //ls;;; ==>只处理一个
+			if(end - start > 0) //ls;;; ==>只处理一个，避免做无用malloc
 			{
 				arr[count] = (char*)malloc(LINELEN);
 				strncpy(arr[count], start , end - start);
@@ -57,8 +57,8 @@ void split2ArrByChar(const char *src, char splitchar, char ** arr, int *num)
 			end++;
 	}
 
-	//处理最后
-	if(end - start > 0)
+	//处理最后. 
+	if(end - start > 0) //避免ls;date;clear;  最后是;时做无用的malloc
 	{
 		arr[count] = (char*)malloc(LINELEN);
 		strncpy(arr[count], start , end - start);
@@ -71,23 +71,26 @@ void split2ArrByChar(const char *src, char splitchar, char ** arr, int *num)
 
 }
 
-int conver2List(const char **buff, char *list[], int *arg_num)
+int conver2List(const char *buff, char *list[], int *arg_num)
 {
-	printf("%s\n",*buff);
+	printf("command:%slxrend\n",buff);//lxrend为了看清楚字符串最后是否有空格
 	int num = 0;
-	const char *end = *buff;
+	const char *end = buff;
 	skipHeadBlank(&end);
 	const char *start = end;
 
-	while(*end != '\0' && *end != ';')  //增加同一行中对；的支持。；表示一个命令的结束。 ls ; date 支持一行中运行多个命令。
+	while(*end != '\0') 
 	{
 		if(*end == ' ' || *end == '\t') 
 		{
-			list[num] = (char*)malloc(sizeof(char)*ARGLEN);
-			strncpy(list[num] , start , end - start);
-			list[num][end - start] = '\0';
-			//printf("list[%d]: %s, left:%s\n",num, list[num],end);
-			num++;
+			if(end - start > 0)
+			{
+				list[num] = (char*)malloc(sizeof(char)*ARGLEN);
+				strncpy(list[num] , start , end - start);
+				list[num][end - start] = '\0';
+				//printf("list[%d]: %s, left:%s\n",num, list[num],end);
+				num++;
+			}
 			end++;
 			skipHeadBlank(&end);
 			start = end;
@@ -98,56 +101,27 @@ int conver2List(const char **buff, char *list[], int *arg_num)
 		}
 	}
  
-	//bug: ；在最后时，会有问题。  空格在最后时也会有问题。
-	//已解决。
-	//对; 或 \0结束时是否还有合法的字符进行检测。 有，则处理。
-	//if(*end == '\0')
-	//{
-		//判断是否是合理的字符。
-		int valid = 0; //有效字符
-		const char *tmp = start;
-		while(*tmp != '\0' && *tmp != ';')
-		{
-			if(*tmp !=' ' && *tmp != '\t')
-			{
-				valid = 1;
-				break;
-			}
-			tmp++;
-		}
-		//还有合法的字符，说明还有命令
-		if(valid)
-		{
-			tmp = start;
-			while(*tmp != '\0' && *tmp != ';')  //增加同一行中对；的支持。
-			{
-				tmp++;
-			}
-			list[num] = (char*)malloc(sizeof(char)*ARGLEN);
-			strncpy(list[num] , start , tmp - start);
-			list[num][tmp - start] = '\0';
-			num++;
-		}
+	if(end - start > 0)  ////避免ls;date;clear\t\t 最后是空格时， 做无用的malloc
+	{
+		list[num] = (char*)malloc(sizeof(char)*ARGLEN);
+		strncpy(list[num] , start , end - start);
+		list[num][end - start] = '\0';
+		num++;
 
-	//}
-	
+	}
 	*arg_num = num;
-
-	if(*end == ';') //skip ;
-		end++;
 	
-	*buff = end;
 	return 0;
 }
 
 int execute(char *args[])
 {
-	if(strcmp(args[0],"exit") == 0)
+	if(strcmp(args[0],"exit") == 0)//不会执行到，在main中就处理了exit
 	{
 		exit(123);
 		printf("not run after exit\n");
 	}
-	if(execvp(args[0] , args) == -1)
+	if(execvp(args[0] , args) == -1) //对exit命令无效
 	{
 		printf("execvp error\n");
 	}
@@ -158,7 +132,7 @@ int execute(char *args[])
 
 int main()
 {
-	char line[81];
+	char line[LINELEN];
 	char *list[MAXARGS];
 	int num_arg;
 	int exitFlag = 0;
@@ -175,22 +149,21 @@ int main()
 			break;
 			//continue;
 		}
+
 		/*
 		先将一行的输入按照分号分割，每个分号是一个命令。shell支持一行多个命令，用分号分割。
 		然后出炉每个命令，将其转成数组。
 		exec 执行。
 		*/
 
-
+		//增加对同一行分号的支持。 分割成一个命令数组
 		split2ArrByChar(line, ';',cmdArr,  &num_cmd);
 
-		printf("%d\n", num_cmd);
+		printf("command num:%d\n", num_cmd);
+		//对命令数组进行处理，执行每一个名命令
 		for(int i = 0; i < num_cmd && exitFlag != 1; i++)
 		{
-			printf("%s\n", cmdArr[i]);
-
 			const char *pc = cmdArr[i];
-
 			skipHeadBlank(&pc);
 			if(*pc == '\0')
 			{
@@ -198,41 +171,45 @@ int main()
 				continue;
 			}
 
-			while(*pc != '\0')
+			conver2List(pc, list, &num_arg);
+			printf("num_arg= %d\n", num_arg);
+			if(num_arg > 0)
 			{
-				conver2List(&pc, list, &num_arg);
-				printf("num_arg= %d\n", num_arg);
-				if(num_arg > 0)
+				if(strcmp(list[0], "exit") == 0)
 				{
-					if(strcmp(list[0], "exit") == 0)
-					{
-						exitFlag = 1;
-						break;		//直接在main进程进行拦截更好，不用fork。
-					}
-					list[num_arg] = NULL;
-					int pid = fork();
-					if(pid < 0)
-					{
-						printf("fork error\n");
-						return -1;
-					}
-					else if(pid == 0)
-					{
-						execute(list);
+					exitFlag = 1;
+					break;		//直接在main进程进行拦截更好，不用fork。
+				}
+				list[num_arg] = NULL;
+				int pid = fork();
+				if(pid < 0)
+				{
+					printf("fork error\n");
+					return -1;
+				}
+				else if(pid == 0)
+				{
+					execute(list);
 
-					}else if (pid > 0)
+				}else if (pid > 0)
+				{
+					signal(SIGINT,SIG_IGN);
+					int status;
+					wait(&status);
+
+					//清理list
+					for(int i = 0; i < num_arg ; i++)
 					{
-						signal(SIGINT,SIG_IGN);
-						int status;
-						wait(&status);
-						//printf("child return exit_code %d, signo %d\n", status >> 8, status & 0x008f );
-						if(status >> 8 == 123)
-							break;
+						if(list[num_arg] != NULL)
+							free(list[num_arg]);
 					}
+					//printf("child return exit_code %d, signo %d\n", status >> 8, status & 0x008f );
+					if(status >> 8 == 123)
+						break;
 				}
 			}
-
 		}
+		//清理malloc的内存
 		for(int i = 0; i < num_cmd ; i++)
 		{
 			if(cmdArr[i] != NULL)
